35baaa18009fae13df6b9dd0f525a10d0395f5a7
Weryson <werysonm.taguary@gmail.com>
2024-04-08 18:37:37 -0300

    added the html inputs

--- eventListeners.js
+++ eventListeners.js
@@ -1,6 +1,13 @@
 const menuBtn = document.querySelector('.settings-btn')
 const settingsMenu = document.querySelector('.settings-menu')
+const closeMenuBtn = document.querySelector('.close-menu-btn')
 
 menuBtn.addEventListener('click', () => {
+  settingsMenu.classList.remove('hidden')
   settingsMenu.classList.add('shown')
+})
+
+closeMenuBtn.addEventListener('click', () => {
+  settingsMenu.classList.remove('shown')
+  settingsMenu.classList.add('hidden')
 })
